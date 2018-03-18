11a2644972ac398770a0ea808cf4a37eaa519082
Akash Vachhani <akash.k.vachhani@gmail.com>
2018-03-17 18:16:36 -0700

    TB Update

--- Lab5/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.srcs/sim_1/new/tb_parallel_multiplier.v
+++ Lab5/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.srcs/sim_1/new/tb_parallel_multiplier.v
@@ -24,46 +24,69 @@ module tb_parallel_multiplier();
 
 	reg  [3:0] A;
 	reg  [3:0] B;
+	reg        clk;
+	reg        rst;
+	reg        en;
 	wire [7:0] P;
 
 	parallel_multiplier_top DUT(
-		.A  (A),
-		.B  (B),
-		.P  (P)
+		.inA  (A),
+		.inB  (B),
+		.P    (P),
+		.clk (clk),
+		.rst (rst),
+		.en  (en)
 		);
 
 	integer i_A;
 	integer i_B;
+	integer clock;
+	integer reset;
+	integer enable;
 
 	integer tb_P;
 	integer error = 0;
 
 	initial begin
-		for(i_A = 0; i_A < 16; i_A = i_A + 1)
-			begin
-				A = i_A;
-				for(i_B = 0; i_B < 16; i_B = i_B + 1)
-				begin
-					B = i_B;
-					tb_P = A * B;
-					#5
-					if (tb_P != P)
-					begin
-						error = error + 1;
-						$display("tb_P = %d AND P = %d", tb_P, P);
-						$display("Error count: %d", error);
-					end
-				end
-			end
-
-		if(error == 0)
-		begin
-			$display("Simulation completed. No errors!");
-		end
-		if(error != 0)
-		begin
-			$display("There were %d errors", error);
-		end
-	end
-
+	    for(clock = 0; clock < 2; clock = clock + 1)
+	       begin
+	           clk = clock;
+	           #5
+	       for(reset = 0; reset < 2; reset = reset + 1)
+	           begin
+	           rst = reset;
+	           #5
+	           for(enable = 0; enable < 2; enable = enable + 1)
+	           begin
+	               en = enable;
+	               #5
+                    for(i_A = 0; i_A < 16; i_A = i_A + 1)
+                        begin
+                            A = i_A;
+                            for(i_B = 0; i_B < 16; i_B = i_B + 1)
+                            begin
+                                B = i_B;
+                                tb_P = A * B;
+                                #5
+                                if (tb_P != P)
+                                begin
+                                    error = error + 1;
+                                    $display("tb_P = %d AND P = %d", tb_P, P);
+                                    $display("Error count: %d", error);
+                                end
+                            end
+                        end
+            
+                    if(error == 0)
+                    begin
+                        $display("Simulation completed. No errors!");
+                    end
+                    if(error != 0)
+                    begin
+                        $display("There were %d errors", error);
+                    end
+                end
+             end
+        end
+    end
 endmodule
