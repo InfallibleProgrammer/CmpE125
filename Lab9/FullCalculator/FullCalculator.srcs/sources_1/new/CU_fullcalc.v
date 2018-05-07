2c80dd2b2eef03f04c0ce8db4bbd2a01755bda42
NickMRamirez94 <bcgnick@gmail.com>
2018-05-06 20:59:03 -0700

    pushing setup of datapath and CU

--- Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/CU_fullcalc.v
+++ Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/CU_fullcalc.v
@@ -1,6 +1,55 @@
 `timescale 1ns / 1ps
 
 module CU_fullcalc(
-
+    input            Go,
+    input      [2:0] F,
+    input            clk,
+    input            rst,
+    input            Done_Calc,
+    input            Done_DIV,
+    input            Div_Err,
+    output reg       Done,
+    output reg       Err,
+    output reg       En_F,
+    output reg       En_X,
+    output reg       En_Y,
+    output reg       Go_Calc,
+    output reg       Op_Calc,
+    output reg       Go_DIV,
+    output reg       Sel_H,
+    output reg       Sel_L,
+    output reg       En_Out_H,
+    output reg       En_Out_L,
+    output reg [3:0] CS 
     );
+    
+    reg [3:0] NS;
+    reg [13:0] out_sig;
+    
+    //state register
+    always @ (posedge clk, posedge rst)
+    begin
+        if (rst) CS <= 0;
+        else CS <= NS;
+    end
+    
+    //output control
+    always @ (out_sig)
+    begin
+    end
+
+    //state transtions
+    always @ (CS, Go)
+    begin
+        case (CS)
+        endcase
+    end
+
+    //output logic
+    always @ (CS)
+    begin
+        case (CS)
+        endcase
+    end
+    
 endmodule
--- Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/DP_fullcalc.v
+++ Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/DP_fullcalc.v
@@ -1,7 +1,25 @@
 `timescale 1ns / 1ps
 
 module DP_fullcalc(
-
+    input [3:0] Xa,
+    input [3:0] Ya,
+    input [2:0] Op_F,
+    input En_F,
+    input En_X,
+    input En_Y,
+    input Go_Calc,
+    input Op_Calc,
+    input Go_DIV,
+    input Sel_H,
+    input Sel_L,
+    input En_Out_H,
+    input En_Out_L,
+    output [3:0] Out_Ha,
+    output [3:0] Out_La,
+    output [2:0] Out_F,
+    output Done_Calc,
+    output Done_DIV,
+    output Div_Err
     );
     
     shift_register F (
@@ -39,6 +57,13 @@ module DP_fullcalc(
         .CLK     (),
         .Q       (),
         .DBZ     ()
+    );
+    
+    mux2 squared (
+        .d1  (),
+        .d2  (),
+        .sel (),
+        .y   ()
     );
     
     FSM_DP small_calc (
