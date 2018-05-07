e58610b4c43186b22acba89db96939197ea2b2da
NickMRamirez94 <bcgnick@gmail.com>
2018-05-06 20:38:52 -0700

    pushing setup of Datapath

--- Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/DP_fullcalc.v
+++ Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/DP_fullcalc.v
@@ -3,4 +3,112 @@
 module DP_fullcalc(
 
     );
+    
+    shift_register F (
+        .D       (),
+        .LeftIn  (1'b0),
+        .RightIn (1'b0),
+        .LD      (),
+        .SL      (0),
+        .SR      (0),
+        .RST     (),
+        .CLK     (),
+        .Q       (),
+        .DBZ     ()
+    );
+    shift_register X (
+        .D       (),
+        .LeftIn  (1'b0),
+        .RightIn (1'b0),
+        .LD      (),
+        .SL      (0),
+        .SR      (0),
+        .RST     (),
+        .CLK     (),
+        .Q       (),
+        .DBZ     ()
+    );
+    shift_register Y (
+        .D       (),
+        .LeftIn  (1'b0),
+        .RightIn (1'b0),
+        .LD      (),
+        .SL      (0),
+        .SR      (0),
+        .RST     (),
+        .CLK     (),
+        .Q       (),
+        .DBZ     ()
+    );
+    
+    FSM_DP small_calc (
+        .Go   (),
+        .Op   (),
+        .In1  (),
+        .In2  (),
+        .clk  (),
+        .CS   (),
+        .Done (),
+        .Out  ()
+    );
+    
+    parallel_multiplier_top mult (
+        .inA (),
+        .inB (),
+        .clk (),
+        .rst (),
+        .en  (),
+        .P   ()
+    );
+    
+    divider div (
+        .Go        (),
+        .rst       (),
+        .clk       (),
+        .Dividend  (),
+        .Divisor   (),
+        .Done      (),
+        .Err       (),
+        .Quotient  (),
+        .Remainder (),
+        .CS        ()
+    );
+    
+    mux2 MUX_H (
+        .d1  (),
+        .d2  (),
+        .sel (),
+        .y   ()
+    );
+    mux2 MUX_L (
+        .d1  (),
+        .d2  (),
+        .sel (),
+        .y   ()
+    );
+    
+    shift_register Out_H (
+        .D       (),
+        .LeftIn  (1'b0),
+        .RightIn (1'b0),
+        .LD      (),
+        .SL      (0),
+        .SR      (0),
+        .RST     (),
+        .CLK     (),
+        .Q       (),
+        .DBZ     ()
+    );
+    shift_register Out_L (
+        .D       (),
+        .LeftIn  (1'b0),
+        .RightIn (1'b0),
+        .LD      (),
+        .SL      (0),
+        .SR      (0),
+        .RST     (),
+        .CLK     (),
+        .Q       (),
+        .DBZ     ()
+    );
 endmodule
--- Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/fullcalc.v
+++ Lab9/FullCalculator/FullCalculator.srcs/sources_1/new/fullcalc.v
@@ -1,7 +1,15 @@
 `timescale 1ns / 1ps
 
 module fullcalc(
-
+    input        Go,
+    input        rst,
+    input        clk,
+    input  [2:0] F,
+    input  [3:0] X,
+    input  [3:0] Y,
+    output       Done,
+    output [3:0] Out_H,
+    output [3:0] Out_L
     );
     
     DP_fullcalc DP_full (
